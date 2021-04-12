e548154b7e906880a50864b74b216cf29f1441da
Kacy Sommers <kacysommers@Kacys-MacBook-Pro.local>
2021-04-11 22:17:31 -0700

    some initial home styling

---
+++ src/app/core/ordering/donuts.ts
@@ -0,0 +1,36 @@
+import donut from '@assets/images/donut_1.jpg';
+
+export enum Donuts {
+  VANILLA = 'Vanilla',
+
+  THAI_TEA = 'Thai Tea',
+
+  NUTELLA = 'Nutella',
+
+  MATCHA = 'Matcha',
+}
+
+export interface IDonut {
+  name: string;
+
+  image: string;
+}
+
+export const donuts: IDonut[] = [
+  {
+    name: Donuts.VANILLA,
+    image: donut,
+  },
+  {
+    name: Donuts.MATCHA,
+    image: donut,
+  },
+  {
+    name: Donuts.THAI_TEA,
+    image: donut,
+  },
+  {
+    name: Donuts.NUTELLA,
+    image: donut,
+  },
+];
--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -1,17 +1,32 @@
 import { RouteComponentProps } from 'react-router-dom';
 import introBg from '@assets/images/sonrisa_1.png';
+import logo from '@assets/images/sonrisa_logo.jpg';
 import styles from './HomePage.module.scss';
+import { donuts } from '../../core/ordering/donuts';
 
 export const HomePage = (props: RouteComponentProps) => {
   return (
-    <div className={styles.landingWrap}>
-      <img
-        src={introBg}
-        alt="Intro Doughnut"
-        style={{
-          width: '100%',
-        }}
-      />
+    <div className={styles.homePageWrap}>
+      <section className={styles.landingWrap}>
+        <div className={styles.landingInner}>
+          <div className={styles.landingLogoWrap}>
+            <img src={logo} alt="Sonrisa Logo" />
+          </div>
+          <div className={styles.landingBgWrap}>
+            <img src={introBg} alt="Intro Doughnut" />
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.orderingSection}>
+        <div className={styles.orderingInner}>
+          {donuts.map((d) => (
+            <div className={styles.donutBox}>
+              <img src={d.image} alt={`Order ${d.name}`} />
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
