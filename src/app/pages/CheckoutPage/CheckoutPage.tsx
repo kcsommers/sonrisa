22c39caada5e95e3040cf0bf50bf462664863b3b
Kacy Sommers <kacysommers@Jonis-MacBook-Pro.local>
2021-05-24 22:40:14 -0700

    not much

--- src/app/pages/CheckoutPage/CheckoutPage.tsx
+++ src/app/pages/CheckoutPage/CheckoutPage.tsx
@@ -3,5 +3,16 @@ import { Header } from './../../components/Header/Header';
 import styles from './CheckoutPage.module.scss';
 
 export const CheckoutPage = (props: RouteComponentProps) => {
-  return <div className={styles.checkoutPageWrap}></div>;
+  return (
+    <div className={styles.checkoutPageWrap}>
+      <div className={styles.checkoutPageInner}>
+        <div className={styles.paymentSection}>
+          <h4>Payment</h4>
+        </div>
+        <div className={styles.orderSection}>
+          <h4>Order</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
