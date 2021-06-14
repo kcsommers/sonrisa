47ec83c5aed57d22d46617abf481b03ae08e4cef
Kacy Sommers <kacy@dev.adaptiva.com>
2021-06-14 16:40:52 -0700

    header link and moving order fetch to app root

--- sonrisa-client/src/app/App.tsx
+++ sonrisa-client/src/app/App.tsx
@@ -1,13 +1,33 @@
 import { useEffect } from 'react';
 import { AppRouter } from './AppRouter';
+import { useOrdering, Api } from '@core';
+import { useDispatch } from 'react-redux';
+import { setOrder } from '@redux';
 
 export const App = () => {
+  const { getOrderId } = useOrdering();
+
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const btns = document.querySelectorAll('button');
     btns.forEach((b) => {
       b.addEventListener('click', () => b.blur());
     });
     return () => {};
+  }, []);
+
+  useEffect(() => {
+    const _orderId = getOrderId();
+
+    if (!_orderId) {
+      return;
+    }
+
+    Api.getOrder(_orderId)
+      .then((res) => dispatch(setOrder(res.data)))
+      .catch((err) => console.error(err));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
--- sonrisa-client/src/app/components/Footer/Footer.tsx
+++ sonrisa-client/src/app/components/Footer/Footer.tsx
@@ -3,7 +3,12 @@ import styles from './Footer.module.scss';
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={`${styles.footerInner}`}></div>
+      <div className={`${styles.footerInner}`}>
+        <div className={`${styles.footerInfoWrap}`}>
+          {/* <h4>Sonrisa</h4>
+          <p>(206) 459-5365</p> */}
+        </div>
+      </div>
     </footer>
   );
 };
