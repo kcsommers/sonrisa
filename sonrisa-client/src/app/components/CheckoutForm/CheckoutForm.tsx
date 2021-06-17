d70da5fd0aa755e3d94fe881b13231ac782badcf
Kacy Sommers <kacysommers@Kacys-MacBook-Pro.local>
2021-06-16 19:16:03 -0700

    initiliazing square sdk

--- sonrisa-client/src/app/components/CheckoutForm/CheckoutForm.tsx
+++ sonrisa-client/src/app/components/CheckoutForm/CheckoutForm.tsx
@@ -1,5 +1,5 @@
 import { Button } from './../Button/Button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './CheckoutForm.module.scss';
 import { RouteComponentProps } from 'react-router';
 
@@ -8,20 +8,46 @@ export const CheckoutForm = (props: RouteComponentProps) => {
 
   const [email, setEmail] = useState('');
 
-  const [cardNumber, setCardNumber] = useState('');
-
   const [phoneNumber, setPhoneNumber] = useState('');
-
-  const [expiration, setExpiration] = useState('');
-
-  const [cvv, setCvv] = useState('');
 
   const submit = () => {
     console.log('NAME:::: ', name);
   };
 
+  useEffect(() => {
+    const initializeCard = async (payments: any) => {
+      const card = await payments.card();
+      await card.attach('#card-container');
+      return card;
+    };
+
+    const initPayments = async () => {
+      const appId = 'sandbox-sq0idb-IKN0-pQaUMYaI8XxPsxWDA';
+      const locationId = 'LFDY60R7H887A';
+
+      const payments = (window as any).Square.payments(appId, locationId);
+      console.log('PAYMENT:::: ', payments);
+      try {
+        const card = await initializeCard(payments);
+        console.log('CArD:::: ', card);
+      } catch (e) {
+        console.error('Initializing Card failed', e);
+        return;
+      }
+    };
+
+    console.log((window as any).Square);
+    if (!(window as any).Square) {
+      throw new Error('Square.js failed to load properly');
+    }
+
+    initPayments();
+  }, []);
+
   return (
     <div className={styles.checkoutFormWrap}>
+      <div id="card-container"></div>
+
       <div className={styles.inputWrap}>
         <input
           type="name"
@@ -44,30 +70,6 @@ export const CheckoutForm = (props: RouteComponentProps) => {
           placeholder="Phone Number"
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
-        />
-      </div>
-      <div className={styles.inputWrap}>
-        <input
-          type="number"
-          placeholder="Card Number"
-          value={cardNumber}
-          onChange={(e) => setCardNumber(e.target.value)}
-        />
-      </div>
-      <div className={styles.inputWrap}>
-        <input
-          type="number"
-          placeholder="Expiration"
-          value={expiration}
-          onChange={(e) => setExpiration(e.target.value)}
-        />
-      </div>
-      <div className={styles.inputWrap}>
-        <input
-          type="number"
-          placeholder="CVV"
-          value={cvv}
-          onChange={(e) => setCvv(e.target.value)}
         />
       </div>
       <div className={styles.inputWrap}>
