6877496489b9e39b22648f5c0351a43809958e14
Kacy Sommers <kacysommers@Kacys-MacBook-Pro.local>
2021-06-16 19:16:03 -0700

    initiliazing square sdk

--- sonrisa-client/src/environments.ts
+++ sonrisa-client/src/environments.ts
@@ -1,14 +1,20 @@
 export const environments = {
   development: {
-    HOST_NAME: '/api',
-    API_BASE_URL: '/api',
+    HOST_NAME: process.env.REACT_APP_HOST_NAME_DEV,
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL_DEV,
+    SQUARE_APP_ID: process.env.REACT_APP_SQUARE_APP_ID_DEV,
+    SQUARE_LOCATION_ID: process.env.REACT_APP_SQUARE_LOCATION_ID_DEV,
   },
   production: {
-    HOST_NAME: 'http://localhost:3000',
-    API_BASE_URL: 'https://www.finjuniorgmapi.com',
+    HOST_NAME: process.env.REACT_APP_HOST_NAME_DEV,
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL_DEV,
+    SQUARE_APP_ID: process.env.REACT_APP_SQUARE_APP_ID_DEV,
+    SQUARE_LOCATION_ID: process.env.REACT_APP_SQUARE_LOCATION_ID_DEV,
   },
   test: {
-    HOST_NAME: 'http://localhost:3000',
-    API_BASE_URL: 'https://www.finjuniorgmapi.com',
+    HOST_NAME: process.env.REACT_APP_HOST_NAME_DEV,
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL_DEV,
+    SQUARE_APP_ID: process.env.REACT_APP_SQUARE_APP_ID_DEV,
+    SQUARE_LOCATION_ID: process.env.REACT_APP_SQUARE_LOCATION_ID_DEV,
   },
 };
