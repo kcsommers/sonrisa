06fe464783ded58719e620806fdf348b97d3fd36
Kacy Sommers <kacysommers@Kacys-MacBook-Pro.local>
2021-06-16 19:27:13 -0700

    adding environments file

---
+++ sonrisa-client/src/environments.ts
@@ -0,0 +1,20 @@
+export const environments = {
+  development: {
+    HOST_NAME: process.env.REACT_APP_HOST_NAME_DEV,
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL_DEV,
+    SQUARE_APP_ID: process.env.REACT_APP_SQUARE_APP_ID_DEV,
+    SQUARE_LOCATION_ID: process.env.REACT_APP_SQUARE_LOCATION_ID_DEV,
+  },
+  production: {
+    HOST_NAME: process.env.REACT_APP_HOST_NAME_DEV,
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL_DEV,
+    SQUARE_APP_ID: process.env.REACT_APP_SQUARE_APP_ID_DEV,
+    SQUARE_LOCATION_ID: process.env.REACT_APP_SQUARE_LOCATION_ID_DEV,
+  },
+  test: {
+    HOST_NAME: process.env.REACT_APP_HOST_NAME_DEV,
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL_DEV,
+    SQUARE_APP_ID: process.env.REACT_APP_SQUARE_APP_ID_DEV,
+    SQUARE_LOCATION_ID: process.env.REACT_APP_SQUARE_LOCATION_ID_DEV,
+  },
+};
