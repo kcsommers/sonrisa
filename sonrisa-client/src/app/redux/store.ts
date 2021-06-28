2a6d5ad216b1326bf1319c276ca8dc96421c0b40
Kacy Sommers <kacysommers@gmail.com>
2021-06-27 21:21:55 -0700

    removing current overlay logic and some references to it

--- sonrisa-client/src/app/redux/store.ts
+++ sonrisa-client/src/app/redux/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { overlayReducer } from './overlay/overylay';
 import { cartReducer } from './cart/cart';
 import { orderReducer } from './order/order.reducer';
 
 export const store = configureStore({
   reducer: {
-    overlay: overlayReducer,
     cart: cartReducer,
     order: orderReducer,
   },
