58f7961c715a213f49d32f11fc8f22b1f78ec50e
Kacy Sommers <kacysommers@gmail.com>
2021-06-27 21:23:44 -0700

    Updates to ordering hook for square integration

--- sonrisa-client/src/app/core/hooks/use-ordering.ts
+++ sonrisa-client/src/app/core/hooks/use-ordering.ts
@@ -1,79 +1,114 @@
-import { setOrderItems, useAppDispatch, useAppSelector } from '@redux';
+import { setOrder, useAppDispatch, useAppSelector } from '@redux';
 import { cloneDeep } from 'lodash';
-import { batch } from 'react-redux';
+import { CatalogObject, Order, OrderLineItem } from 'square';
 import { Api } from '../api/api';
+import { logger } from '../logger';
+import { getItemVariationId } from '../utils';
 import { useStorage } from './use-storage';
-import { setOrderId } from './../../redux/order/order';
-import { Order, OrderLineItem } from 'square';
 
 export interface IOrderingHook {
   orderState: Order | undefined;
 
-  updateOrder: (item: OrderLineItem, quantity: number) => void;
+  getOrderId: () => string;
 
-  getOrderId: () => string | null;
+  getOrderById: (orderId: string) => Promise<Order>;
+
+  getItemQuantity: (itemId: string) => number;
+
+  setItemQuantity: (item: CatalogObject, quantity: number) => Promise<Order>;
 }
 
 export const useOrdering = (): IOrderingHook => {
   const orderState = useAppSelector((state) => state.order);
 
+  const { setSessionItem, getSessionItem, storageKeys } = useStorage();
+
   const dispatch = useAppDispatch();
 
-  const { storageKeys, setSessionItem, getSessionItem } = useStorage();
-
-  const getOrderId = (): string | null => {
-    return getSessionItem(storageKeys.ORDER_NUMBER);
+  const getOrderId = (): string => {
+    const _orderId = getSessionItem(storageKeys.ORDER_NUMBER);
+    return _orderId || '';
   };
 
-  const createOrder = (order: Order) => {
-    Api.createOrder(order)
-      .then((res) => {
-        const _order = res.data;
-        // setSessionItem(storageKeys.ORDER_NUMBER, _order._id);
-      })
-      .catch((err) => console.error(err));
+  const getOrderById = async (orderId: string): Promise<Order> => {
+    const _response = await Api.getOrder(orderId);
+    return _response.data;
   };
 
-  const updateOrder = (item: OrderLineItem, quantity: number): void => {
-    if (!orderState) {
-      return;
+  const getItemQuantity = (itemId: string): number => {
+    if (
+      !orderState ||
+      !(orderState as any).line_items ||
+      !(orderState as any).line_items.length
+    ) {
+      return 0;
     }
 
-    // const _clonedItems = cloneDeep(orderState.items);
+    const _item = (orderState as any).line_items.find(
+      (item: any) => (item as any).catalog_object_id === itemId
+    );
+    if (!_item) {
+      return 0;
+    }
 
-    // // look for item in cart
-    // let _orderItemIndex = _clonedItems.findIndex((i) => i.item.id === item.id);
+    return +_item.quantity;
+  };
 
-    // // if it exists, set the quantity
-    // if (_orderItemIndex > -1) {
-    //   _clonedItems[_orderItemIndex].quantity = quantity;
-    // } else {
-    //   // otherwise create new item
-    //   _clonedItems.push({
-    //     item: item,
-    //     quantity: quantity,
-    //   });
-    // }
+  const setItemQuantity = async (
+    item: CatalogObject,
+    quantity: number
+  ): Promise<Order> => {
+    // make a copy of the current order items
+    const _clonedItems = cloneDeep((orderState as any)?.line_items || []);
+    console.log('[cloned items]:::: ', orderState, _clonedItems);
+    // look for the item being updated
+    let _lineItem = _clonedItems.find((i: any) => {
+      console.log('item:::: ', i.catalog_object_id, getItemVariationId(item));
+      return i.catalog_object_id === getItemVariationId(item);
+    });
 
-    // Api.updateOrder(orderState._id, _clonedItems)
-    //   .then((res) => {
-    //     const _order = res.data;
-    //     setSessionItem(storageKeys.ORDER_NUMBER, _order._id);
+    console.log('_LINE', _lineItem);
 
-    //     batch(() => {
-    //       dispatch(setOrderItems(_clonedItems));
+    // if it exists just update the quantity
+    if (_lineItem) {
+      _lineItem.quantity = String(quantity);
+    } else {
+      // otherwise create a new line item and push it into the items array
+      _lineItem = {
+        quantity: String(quantity),
+        catalogObjectId: getItemVariationId(item),
+      };
 
-    //       if (!orderState._id) {
-    //         dispatch(setOrderId(_order._id));
-    //       }
-    //     });
-    //   })
-    //   .catch((err) => console.error(err));
+      _clonedItems?.push(_lineItem);
+    }
+
+    console.log('cloned items:::: ', _clonedItems);
+
+    try {
+      // if theres an existing id update the order
+      // otherwise create a new one
+      const _response = orderState?.id
+        ? await Api.updateOrder(
+            orderState?.id,
+            orderState?.version || 0,
+            _clonedItems
+          )
+        : await Api.createOrder(_clonedItems);
+
+      setSessionItem(storageKeys.ORDER_NUMBER, _response.data.id);
+      dispatch(setOrder(_response.data));
+      return _response.data;
+    } catch (err) {
+      logger.error(err);
+      throw new Error(err);
+    }
   };
 
   return {
     orderState,
-    updateOrder,
     getOrderId,
+    getOrderById,
+    getItemQuantity,
+    setItemQuantity,
   };
 };
