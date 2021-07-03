1211a1e5e0713ba9efb8ad45ff0c3fdae806f07c
Kacy Sommers <kacysommers@gmail.com>
2021-07-03 09:10:09 -0700

    Adding receipt link to order success page

--- sonrisa-client/src/app/pages/OrderSuccessPage/OrderSuccessPage.tsx
+++ sonrisa-client/src/app/pages/OrderSuccessPage/OrderSuccessPage.tsx
@@ -1,13 +1,23 @@
-import { RouteComponentProps } from 'react-router';
+import { RouteComponentProps, useLocation } from 'react-router';
+import { Payment } from 'square';
 import styles from './OrderSuccessPage.module.scss';
 
-export const OrderSuccessPage = (props: RouteComponentProps) => {
+interface ISuccessPageLocationState {
+  payment: Payment;
+}
+
+export const OrderSuccessPage = () => {
+  const { state } = useLocation<ISuccessPageLocationState>();
+
   return (
     <div>
       <div className={styles.pageInner}>
         <div className={`${styles.messageWrap} responsive-container`}>
           <h4>Thank you!</h4>
           <h3>Your order has been placed</h3>
+          <a href={state.payment.receiptUrl}>
+            Click here to view your receipt.
+          </a>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
             ratione repellat dolore soluta facilis nam molestiae? Minus eius
