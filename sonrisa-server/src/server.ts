258223b427fb7b4d37a4cd83d3afc35fb97fd2d4
Kacy Sommers <kacysommers@gmail.com>
2021-07-11 21:11:39 -0700

    Removing all db and boilerplate related code and dependencies

--- sonrisa-server/src/server.ts
+++ sonrisa-server/src/server.ts
@@ -1,18 +1,11 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import connectDB from '../config/database';
-import auth from './routes/api/auth';
 import order from './routes/api/order';
-import profile from './routes/api/profile';
-import user from './routes/api/user';
 import catalog from './routes/api/catalog';
 import contact from './routes/api/contact';
 import instagram from './routes/api/instagram';
 
 const app = express();
-
-// Connect to MongoDB
-connectDB();
 
 // Express configuration
 app.set('port', process.env.PORT || 5000);
@@ -26,9 +19,6 @@ app.get('/', (_req, res) => {
   res.send('API Running');
 });
 
-app.use('/api/auth', auth);
-app.use('/api/user', user);
-app.use('/api/profile', profile);
 app.use('/api/order', order);
 app.use('/api/catalog', catalog);
 app.use('/api/contact', contact);
