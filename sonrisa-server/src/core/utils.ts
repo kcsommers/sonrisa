d029761ad630df0a6e46341d43d9a0f83e180018
Kacy Sommers <kacysommers@gmail.com>
2021-07-22 23:00:11 -0700

    removing camelcaseKeys lib, implementing own method

--- sonrisa-server/src/core/utils.ts
+++ sonrisa-server/src/core/utils.ts
@@ -1,14 +1,5 @@
-import camelcaseKeys from 'camelcase-keys';
-import { CatalogObject, Order, OrderLineItem } from 'square';
-
-export const getCamelcaseKeys = <T>(data: any): T => {
-  try {
-    const _res = <T>camelcaseKeys(data, { deep: true });
-    return _res;
-  } catch (err) {
-    return null;
-  }
-};
+import { camelCase } from 'lodash';
+import { CatalogObject } from 'square';
 
 export const getItemVariationId = (item: CatalogObject): string => {
   return item.itemData?.variations?.[0].id || '';
@@ -16,4 +7,32 @@ export const getItemVariationId = (item: CatalogObject): string => {
 
 export const getItemImageId = (item: CatalogObject): string => {
   return item.imageId;
+};
+
+export const camelcaseKeys = <T>(o: T): any => {
+  let newO, origKey, newKey, value;
+  if (o instanceof Array) {
+    return o.map(function (value) {
+      if (typeof value === 'object') {
+        value = camelcaseKeys(value);
+      }
+      return value;
+    });
+  } else {
+    newO = {};
+    for (origKey in o) {
+      if (o.hasOwnProperty(origKey)) {
+        newKey = camelCase(origKey);
+        value = o[origKey];
+        if (
+          value instanceof Array ||
+          (value !== null && value.constructor === Object)
+        ) {
+          value = camelcaseKeys(value);
+        }
+        newO[newKey] = value;
+      }
+    }
+  }
+  return newO;
 };
