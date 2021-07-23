3d843c2efb87d39c245b3c9ab05cc3ac4f32b67f
Kacy Sommers <kacysommers@gmail.com>
2021-07-22 23:12:51 -0700

    path fix

--- sonrisa-server/src/core/public.ts
+++ sonrisa-server/src/core/public.ts
@@ -2,7 +2,7 @@ export { NotAcceptingOrdersReasons } from './orders/NotAcceptingOrdersReasons';
 export { catalogConstants } from './catalog/catalog-constants';
 export { catalogImages } from './catalog/catalog-images';
 export { OrderStates } from './orders/OrderStates';
-export { Doughnuts } from './catalog/doughnuts';
+export { Doughnuts } from './catalog/Doughnuts';
 export { IContactResponse } from './contact/IContactResponse';
 export { IContactRequest } from './contact/IContactRequest';
 export { IInstagramMedia } from './instagram/IInstagramMedia';
