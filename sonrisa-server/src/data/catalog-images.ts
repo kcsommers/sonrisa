f18d5ff76ab337280f6f5ba6261a75e43f0e021f
Kacy Sommers <kacysommers@gmail.com>
2021-12-03 22:38:31 -0800

    add photo paths

---
+++ sonrisa-server/src/data/catalog-images.ts
@@ -0,0 +1,32 @@
+export const catalogImages = {
+  development: {
+    // vanilla
+    NO64X2IBHQJA67BLARL2J7AT: [
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455065/Sonrisa/vanilla-2.jpg',
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455065/Sonrisa/vanilla-3.jpg',
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455065/Sonrisa/vanilla-4.jpg',
+    ],
+    // matcha
+    QKZWNLHASJKT7WUONVZXABCI: [
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455068/Sonrisa/matcha-2.jpg',
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455068/Sonrisa/matcha-3.jpg',
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455068/Sonrisa/matcha-4.jpg',
+    ],
+    // nutella
+    B7F6XU3IPNYPR4HXBP5GBJEN: [
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455067/Sonrisa/nutella-2.jpg',
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455067/Sonrisa/nutella-3.jpg',
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455067/Sonrisa/nutella-4.jpg',
+    ],
+    // thai
+    VQ7HS4EXUN7ZTNAQPZTYZUIO: [
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455067/Sonrisa/thai-2.jpg',
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455067/Sonrisa/thai-3.jpg',
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455067/Sonrisa/thai-4.jpg',
+    ],
+    // strawberry
+    Y5CMOCXDDJ5ENZQ7BO7GVQ6F: [
+      'https://res.cloudinary.com/kcsommers/image/upload/v1625455068/Sonrisa/strawberry-2.jpg',
+    ],
+  },
+};
